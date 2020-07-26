9484772168bc00c484e488b87b53068f11b4c7aa
Moses <gabe.susman@gmail.com>
2020-07-26 05:23:07 -0600

    first commit

---
+++ PageViewOnXcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PageViewOnXcode
+//
+//  Created by Moses on 7/26/20.
+//  Copyright © 2020 Max Susman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
